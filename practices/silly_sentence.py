6b72071c2cbd39d5d0b9216aa11963d91126eb81
ian.lowry <ian.lowry@ucas-edu.net>
2025-09-10 13:41:34 -0600

    silly sentences

---
+++ practices/silly_sentence.py
@@ -0,0 +1,9 @@
+#IL 6th silly sentences
+
+name=input("please type a name : ").title().strip()
+verb=input("please type a verb without an ing : ").title().strip()
+noun=input("please type a noun : ").title().strip()
+number=input('please type a number between 1 and 24 : ').title().strip()
+item= input('please type a random object : ').title().strip()
+print(name+' fell down the stairs and got head trama so he '+verb+' to the docters office the docter perscribed '+number+' '+item+' a day for a year')
+print('thanks for trying my mad lib (:')
