c30ff813c3dbff04a1b4a19329a53633b9d6840d
ian.lowry <ian.lowry@ucas-edu.net>
2025-09-17 13:21:39 -0600

    trashed tic tac toe hampster better silly names

---
+++ hamster.py
@@ -0,0 +1,4 @@
+name=input('what is your name?')
+decor1=input('hello '+name+' how do you want to decorate your name on the left? : ')
+decor2=input('how do you want to decorate your name on the right? : ')
+print(decor1+name+decor2)
--- practices/silly_names.py
+++ practices/silly_names.py
@@ -1,3 +1,3 @@
 #SP 6th name decorator
 name = input("type your name here : ").strip().title()
-print ('(:'+name+':)')
+print ('(: '+name+' :)')
