d499d18ba7ab81f107d4adda59c97ddf835ebb2d
ian.lowry <ian.lowry@ucas-edu.net>
2025-09-17 12:49:23 -0600

    tic tac toe

---
+++ practices/hello_world.py
@@ -0,0 +1,15 @@
+#IL 6th update hello world
+
+def hello(name):
+    print(f"hello "+name+"!")
+
+name=input("what is your name : ").title().strip()
+hello(name)
+name=input("what is your name : ").title().strip()
+hello(name)
+name=input("what is your name : ").title().strip()
+hello(name)
+name=input("what is your name : ").title().strip()
+hello(name)
+name=input("what is your name : ").title().strip()
+hello(name)
