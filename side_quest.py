6c3c6762439695eb9b7ba4fe7057c2ccd1419302
ianlowryucas <ian.lowry@ucas-edu.net>
2025-09-19 13:24:38 -0600

    side quest

---
+++ side_quest.py
@@ -0,0 +1,7 @@
+import datetime
+current=datetime.datetime.now()
+hour=current.hour
+print(f'the time is: {current}')
+print(f'the hour is: {hour}')
+
+kaboom = ['scout', 'pyro', 'demoman', 'spy', 'sniper', 'soldier', 'heavy', 'engineer', 'medic', 'red', 'blu']
