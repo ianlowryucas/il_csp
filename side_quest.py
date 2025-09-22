6d72c78b84ded3de4b2d4d72e20181cd9d61d2b2
ianlowryucas <ian.lowry@ucas-edu.net>
2025-09-22 13:41:39 -0600

    side questier

--- side_quest.py
+++ side_quest.py
@@ -3,4 +3,26 @@ current=datetime.datetime.now()
 hour=current.hour
 print(f'the time is: {current}')
 print(f'the hour is: {hour}')
-kaboom = ['scout', 'soldier', 'pyro', 'demoman', 'heavy', 'engineer', 'medic', 'sniper', 'spy', 'red', 'blu']
+robert = input ('name a tf2 subclass : ')
+kaboom = ['scout', 'soldier', 'pyro', 'demoman', 'heavy', 'engineer', 'medic', 'sniper', 'spy', 'red', 'blu', robert]
+print (kaboom[11], kaboom[10], kaboom[9], kaboom[8], kaboom[7], kaboom[6], kaboom[5], kaboom[4], kaboom[3], kaboom[2], kaboom[1], kaboom[0])
+
+tf2 = kaboom[11]
+
+for kaboom in tf2:
+    print(f'TF2 {kaboom}')
+print('and part three we have to increase our iderator these are the three steps i timed that dont think other wise i am not friends with zeus maybe thor but probably not while i is less than 21 we have to increase our iderator')
+i = 1
+#while i > 0:
+    #print(i)
+    #i+=1
+
+import random
+num = random.randint(1,200)
+x=1
+while x != num:
+    print('your mom')
+    x+=1
+
+print('your dad')
+print(num)
