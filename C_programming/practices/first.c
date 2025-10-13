8dae2bd38740668746a931bec914cbfa77a72ee0
ianlowryucas <ian.lowry@ucas-edu.net>
2025-10-13 13:27:54 -0600

    first

---
+++ C_programming/practices/first.c
@@ -0,0 +1,12 @@
+//il 6th first program
+
+#include <stdio.h>
+
+int main(void){
+    char name[20];
+    printf("type your name : ");
+    scanf("%s", &name);
+    printf("hello %s", name);
+    return 0;
+}
+
