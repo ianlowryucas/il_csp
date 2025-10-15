2cd43b3775256441d332af66b106a29e8a681693
ianlowryucas <ian.lowry@ucas-edu.net>
2025-10-15 13:39:03 -0600

    silly names fix

---
+++ C_programming/practices/hello_world_update.c
@@ -0,0 +1,17 @@
+// il 6th asingment
+#include <stdio.h>
+char name[20];
+void hi(char* name){
+    printf("type a name : ");
+    scanf("%s", &name);
+    printf("hello %s!", name);
+}
+int main(void){
+    hi;
+    hi;
+    hi;
+    hi;
+    hi;
+    return 0;
+}
+
--- C_programming/practices/silly_names.c
+++ C_programming/practices/silly_names.c
@@ -1,11 +1,17 @@
 //il 6th asingment
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
     char name[20];
+    char right = (":)");
+    char left = ("(:");
+
     printf("type your name : ");
     scanf("%s", &name);
-    printf("(: %s :)", name);
+    strcat(name, right);
+    strcat(left, name);
+    printf("%s", left);
 
     return 0;
 }
