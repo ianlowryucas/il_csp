307dbaa3afd59585f9bafcb9c6b33607beccccfe
ianlowryucas <ian.lowry@ucas-edu.net>
2025-10-15 13:12:46 -0600

    silly sentence

---
+++ C_programming/practices/silly_names.c
@@ -0,0 +1,12 @@
+//il 6th asingment
+#include <stdio.h>
+
+int main(void){
+    char name[20];
+    printf("type your name : ");
+    scanf("%s", &name);
+    printf("(: %s :)", name);
+
+    return 0;
+}
+
